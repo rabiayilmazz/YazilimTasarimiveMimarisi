bdb26717f353a6dba9c5f1773f597b750cfb2eed
rabia yılmaz <rabiarabiabbb@gmail.com>
2020-12-24 18:24:07 +0300

    alt klasör de değişiklik yapıldı

---
+++ OOP/src/oop/OOP.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop;
+
+/**
+ *
+ * @author Rabia
+ */
+public class OOP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Kitap kitap= new Kitap();
+        kitap.isim = "oop";
+        kitap.ismiGöster();
+    }
+    
+}
