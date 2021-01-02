ed34a94f835160483d955429a446de9083b1088e
rabia yılmaz <rabiarabiabbb@gmail.com>
2021-01-02 15:26:20 +0300

    singleton tasarım deseni

--- Singleton/src/App.java
+++ Singleton/src/App.java
@@ -11,8 +11,8 @@
 
 public class App {
     public static void main(String[] args) {
-        Database firstInstanceRequest = Database.getInstance();
-        Database secondInstanceRequest = Database.getInstance();
+        Database firstInstanceRequest = Database.getInstance();// İlk oluşturulan nesne
+        Database secondInstanceRequest = Database.getInstance();// ikinci oluşturulan nesne
 
         Boolean isEquals = firstInstanceRequest == secondInstanceRequest;
 
