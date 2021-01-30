f67efd215011840737409f9a0a4d596910782f17
rabia yılmaz <rabiarabiabbb@gmail.com>
2021-01-30 12:30:07 +0300

    rsa kod

--- Rsa/src/com/sanfoundry/setandstring/Rsa.java
+++ Rsa/src/com/sanfoundry/setandstring/Rsa.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.math.BigInteger;
 import java.util.Random;
 
- 
-
 public class Rsa
 
 {
@@ -18,7 +16,6 @@ public class Rsa
     private BigInteger d;
     private int        bitlength = 1024;
     private Random     r;
-
     public Rsa()
     {
         r = new Random();
@@ -38,14 +35,16 @@ public class Rsa
         this.e = e;
         this.d = d;
         this.N = N;
+        
     }
     @SuppressWarnings("deprecation")
     public static void main(String[] args) throws IOException
     {
         Rsa rsa = new Rsa();
+        System.out.println(rsa.toString());
         DataInputStream in = new DataInputStream(System.in);
         String teststring;
-        System.out.println("Enter the plain text:");
+        System.out.println("Şifrelenecek metni giriniz:");
         teststring = in.readLine();
         deneme(teststring);
         
@@ -60,27 +59,38 @@ public class Rsa
         }
         return test;
     }
-
-    // Encrypt message
+    
     public byte[] encrypt(byte[] message)
     {
         return (new BigInteger(message)).modPow(e, N).toByteArray();
     }
-    // Decrypt message
     public byte[] decrypt(byte[] message)
     {
         return (new BigInteger(message)).modPow(d, N).toByteArray();
     }
      private static void deneme(String teststring) {
-         Rsa rsa = new Rsa();
-        System.out.println("Encrypting String: " + teststring);
-        System.out.println("String in Bytes: "
-                + bytesToString(teststring.getBytes()));
+        Rsa rsa = new Rsa();
+        System.out.println("Şifrelenecek string: " + teststring);
+        System.out.println("Stringin byte degeri: " + bytesToString(teststring.getBytes()));
         // encrypt
         byte[] encrypted = rsa.encrypt(teststring.getBytes());
         // decrypt
         byte[] decrypted = rsa.decrypt(encrypted);
-        System.out.println("Decrypting Bytes: " + bytesToString(decrypted));
-        System.out.println("Decrypted String: " + new String(decrypted));
+        //System.out.println("sonuc String: " + bytesToString(teststring.getBytes()));
+        System.out.println("Şifrelenmiş byte: " + bytesToString(encrypted));
+        System.out.println("Deşifrelenecek byte: " + bytesToString(decrypted));
+        System.out.println("Deşifrelenmiş string: " + new String(decrypted));
+                
+    }
+     
+     
+    /*@Override
+    public String toString() {
+        return "Public değerler: \n" + "N=" + N + ", \ne=" + e + '}';
+    }*/
+
+    @Override
+    public String toString() {
+        return "Rsa{" + "p=" + p + ", \nq=" + q + ", \nN=" + N + ", \nphi=" + phi + ", \ne=" + e + ", \nd=" + d + ", \nbitlength=" + bitlength + ", \nr=" + r + '}';
     }
 }
